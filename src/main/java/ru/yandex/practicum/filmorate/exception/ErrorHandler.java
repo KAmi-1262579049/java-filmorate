8258b5764a35f4b10111ad48276f2ed06ca4091c
KAmi-1262579049 <camisha21@yandex.ru>
2026-06-01 14:07:42 +0300

    Update ErrorHandler.java

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 
 @Slf4j
 @RestControllerAdvice
