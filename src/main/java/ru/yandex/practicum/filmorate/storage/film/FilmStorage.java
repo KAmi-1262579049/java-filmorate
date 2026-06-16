d4805bb8e3b5731cf7ae2ddbfb6831fc0cbf7c11
KAmi-1262579049 <camisha21@yandex.ru>
2026-06-16 10:47:25 +0300

    Update FilmStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.filmorate.storage.film;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 // Интерфейс FilmStorage описывает операции хранения фильмов
 public interface FilmStorage {
@@ -16,9 +18,12 @@ public interface FilmStorage {
     // Удаляет фильм по id
     void delete(int id);
 
-    // Возвращает фильм по id
-    Film getById(int id);
+    // Возвращает фильм по id, если он есть в хранилище
+    Optional<Film> getById(int id);
 
     // Возвращает все фильмы
     Collection<Film> findAll();
+
+    // Возвращает count самых популярных фильмов по количеству лайков
+    List<Film> getPopularFilms(int count);
 }
