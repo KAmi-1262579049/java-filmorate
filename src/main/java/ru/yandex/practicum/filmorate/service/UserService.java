0c8746d49c1e819317bf4e3ed2f3639053ae1d43
KAmi-1262579049 <camisha21@yandex.ru>
2026-06-16 10:46:45 +0300

    Update UserService.java

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -3,9 +3,12 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 
@@ -25,17 +28,23 @@ public class UserService {
 
     // Создаёт пользователя
     public User create(User user) {
+        validate(user);
+        fillName(user);
         return userStorage.create(user);
     }
 
     // Обновляет пользователя
     public User update(User user) {
+        validate(user);
+        getById(user.getId());
+        fillName(user);
         return userStorage.update(user);
     }
 
     // Возвращает пользователя по id
     public User getById(int id) {
-        return userStorage.getById(id);
+        return userStorage.getById(id)
+                .orElseThrow(() -> new NotFoundException("Пользователь с id=" + id + " не найден"));
     }
 
     // Возвращает всех пользователей
@@ -43,10 +52,17 @@ public class UserService {
         return userStorage.findAll();
     }
 
+    // Удаляет пользователя
+    public void delete(int id) {
+        getById(id);
+        userStorage.delete(id);
+    }
+
     // Добавляет пользователей друг другу в друзья
     public void addFriend(int userId, int friendId) {
-        User user = userStorage.getById(userId);
-        User friend = userStorage.getById(friendId);
+        checkDifferentUsers(userId, friendId);
+        User user = getById(userId);
+        User friend = getById(friendId);
         user.getFriends().add(friendId);
         friend.getFriends().add(userId);
         log.info("Пользователи с id={} и id={} стали друзьями", userId, friendId);
@@ -54,8 +70,9 @@ public class UserService {
 
     // Удаляет пользователей из друзей друг у друга
     public void removeFriend(int userId, int friendId) {
-        User user = userStorage.getById(userId);
-        User friend = userStorage.getById(friendId);
+        checkDifferentUsers(userId, friendId);
+        User user = getById(userId);
+        User friend = getById(friendId);
         user.getFriends().remove(friendId);
         friend.getFriends().remove(userId);
         log.info("Пользователи с id={} и id={} больше не друзья", userId, friendId);
@@ -63,19 +80,50 @@ public class UserService {
 
     // Возвращает список друзей пользователя
     public List<User> getFriends(int userId) {
-        User user = userStorage.getById(userId);
+        User user = getById(userId);
         return user.getFriends().stream()
-                .map(userStorage::getById)
+                .map(this::getById)
                 .toList();
     }
 
     // Возвращает список общих друзей двух пользователей
     public List<User> getCommonFriends(int userId, int otherId) {
-        User user = userStorage.getById(userId);
-        User otherUser = userStorage.getById(otherId);
+        checkDifferentUsers(userId, otherId);
+        User user = getById(userId);
+        User otherUser = getById(otherId);
         return user.getFriends().stream()
                 .filter(otherUser.getFriends()::contains)
-                .map(userStorage::getById)
+                .map(this::getById)
                 .toList();
+    }
+
+    // Проверяет, что пользователь не пытается выполнить операцию сам с собой
+    private void checkDifferentUsers(int userId, int otherId) {
+        if (userId == otherId) {
+            throw new ValidationException("Операция должна выполняться с разными пользователями");
+        }
+    }
+
+    // Заполняет имя логином, если имя не указано
+    private void fillName(User user) {
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+    }
+
+    // Проверяет корректность данных пользователя
+    private void validate(User user) {
+        if (user == null) {
+            throw new ValidationException("Пользователь не передан");
+        }
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            throw new ValidationException("Email не может быть пустым и должен содержать @");
+        }
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Логин не может быть пустым или содержать пробелы");
+        }
+        if (user.getBirthday() == null || user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем");
+        }
     }
 }
